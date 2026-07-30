8e5f5fbe7020093b89c9afbe5bc1b4112789b0fc
Mohammad Osama <ansariosama024@gmail.com>
2026-07-30 20:42:19 +0530

    Time: 0 ms (100%), Space: 20.9 MB (30.04%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,28 +4,24 @@ public:
 
         int m = matrix.size();
         int n = matrix[0].size();
-
         int col0 = 1;
-
-        // Step 1: Mark rows and columns
-        for (int i = 0; i < m; i++) {
-
-            if (matrix[i][0] == 0)
-                col0 = 0;
-
-            for (int j = 1; j < n; j++) {
-
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                
+                if(j!=0){
+                    matrix[0][j]=0;
+                }
+                else {
+                    col0=0;
+                }
                 }
             }
         }
-
-        // Step 2: Fill zeros using markers
         for (int i = m - 1; i >= 0; i--) {
 
-            for (int j = n - 1; j >= 1; j--) {
+            for (int j = n - 1; j >0; j--) {
 
                 if (matrix[i][0] == 0 || matrix[0][j] == 0)
                     matrix[i][j] = 0;
