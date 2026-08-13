8ba418fa37cb65bc310dfe19324514456201a08c
Mohammad Osama <ansariosama024@gmail.com>
2026-08-13 22:41:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (51.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(1LL*mid*mid<=x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+};
